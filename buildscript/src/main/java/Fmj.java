39d97bf773b1470ca91b777501fd3ab5b3ff35a8
maximumpower55 <maximumpowermc@gmail.com>
2023-07-09 17:27:39 -0700

    oops

--- buildscript/src/main/java/Fmj.java
+++ buildscript/src/main/java/Fmj.java
@@ -4,7 +4,7 @@ import java.util.Locale;
 
 public class Fmj {
 	public static final String ID = "tropics";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1";
 	public static final String NAME = "Tropics";
 	public static final String DESCRIPTION = "A mod about palm trees and coconuts!";
 	public static final String[] AUTHORS = new String[] { "Maximum (Author)", "Octal (Texture Artist)" };
@@ -12,7 +12,7 @@ public class Fmj {
 	public static class Contacts {
 		public static final String SOURCES = "https://github.com/maximmods/Tropics";
 		public static final String ISSUES = SOURCES + "/issues";
-		public static final String DISCORD = "https://discord.gg/TchSsQTwct";
+		public static final String DISCORD = "https://discord.gg/ZN6GAtbr3U";
 	}
 
 	public static final String LICENSE = "MIT";
