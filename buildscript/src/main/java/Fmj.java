f5e3115cb7cfdcddc9ff0a51e1576436e775397d
maximumpower55 <maximumpowermc@gmail.com>
2023-07-09 17:14:32 -0700

    Bump stuff

--- buildscript/src/main/java/Fmj.java
+++ buildscript/src/main/java/Fmj.java
@@ -4,7 +4,7 @@ import java.util.Locale;
 
 public class Fmj {
 	public static final String ID = "tropics";
-	public static final String VERSION = "0.1.6";
+	public static final String VERSION = "1.0.0";
 	public static final String NAME = "Tropics";
 	public static final String DESCRIPTION = "A mod about palm trees and coconuts!";
 	public static final String[] AUTHORS = new String[] { "Maximum (Author)", "Octal (Texture Artist)" };
